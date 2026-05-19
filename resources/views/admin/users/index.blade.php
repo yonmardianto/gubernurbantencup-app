9ab693d02a2d6fd47cdc21ccb946d4d3fafcfbb0
Yon Mardianto <yongithub@gmail.com>
2026-05-19 16:50:09 +0700

    fix unlock list

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -297,7 +297,7 @@
                                 actions +=
                                     `<button type="button" class="dropdown-item text-danger" data-bs-toggle="modal" data-bs-target="#deleteUserModal" data-user-id="${row.id}" data-user-name="${row.name}"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v1" /></svg> &nbsp; Archieve </button>`;
 
-                                if (row.manual_unlock === 1) {
+                                if (row.manual_unlock == "1") {
                                     actions +=
                                         `<button type="button" class="dropdown-item text-danger" data-bs-toggle="modal" data-bs-target="#userActionModal" data-user-id="${row.id}" data-user-name="${row.name}" data-user-club="${row.club}" data-action="lock"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user-cancel"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /><path d="M19 19m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M17 21l4 -4" /></svg> &nbsp; Lock </button>`;
 
